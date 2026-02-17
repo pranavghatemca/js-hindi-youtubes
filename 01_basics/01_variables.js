5050c2b31d29cac576a80963cee076701a996f2e
Ghate Pranav Nitin <pranav_ghate_mca@moderncoe.edu.in>
2026-02-17 10:46:47 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+ const accountId = 144553
+ let accountEmail = "pranavghate@gmail.com"
+ var accountPassword= "123"
+ accountCity = "Pune"
+ let accountState;
+
+ //accountId  = 2 not allowed because accountID is a constant variable
+
+accountEmail = "pranav123@gmail.com"
+ console.log(accountId);
+
+ console.table([accountEmail, accountId, accountPassword, accountCity, accountState]);
